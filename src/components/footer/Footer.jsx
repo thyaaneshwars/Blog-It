f65718669073d25bb5869e1abfe0736a8a733917
thyaaneshwars <84958811+thyaaneshwars@users.noreply.github.com>
2024-11-25 18:28:30 +0530

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,143 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../Logo'
+
+function Footer() {
+  return (
+    <section className="overflow-hidden pt-10 pb-3 bg-[#00040F] ">
+      <div className="mx-auto max-w-7xl px-4">
+        <div className="-m-6 flex flex-wrap">
+          <div className="w-full p-6 md:w-full lg:w-5/12">
+            <div className="flex h-full flex-col justify-between">
+              <div className="mb-4 inline-flex items-center">
+                <Logo width="100px" />
+              </div>
+              <div>
+                <p className="text-sm text-gray-600">
+                  &copy; Copyright 2024. All Rights Reserved By Shiva.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="w-full p-6 md:w-1/3 lg:w-2/12">
+            <div className="h-full">
+              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-white">
+                Company
+              </h3>
+              <ul>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-white/60 hover:text-white"
+                    to="/"
+                  >
+                    Features
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-white/60 hover:text-white"
+                    to="/"
+                  >
+                    Pricing
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-white/60 hover:text-white"
+                    to="/"
+                  >
+                    Affiliate Program
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className=" text-base font-medium text-white/60 hover:text-white"
+                    to="/"
+                  >
+                    Press Kit
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="w-full p-6 md:w-1/3 lg:w-2/12">
+            <div className="h-full">
+              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-white">
+                Support
+              </h3>
+              <ul>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-white/60 hover:text-white"
+                    to="/"
+                  >
+                    Account
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-white/60 hover:text-white"
+                    to="/"
+                  >
+                    Help
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-white/60 hover:text-white"
+                    to="/"
+                  >
+                    Contact Us
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className=" text-base font-medium text-white/60 hover:text-white"
+                    to="/"
+                  >
+                    Customer Support
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="w-full p-6 md:w-1/3 lg:w-3/12">
+            <div className="h-full">
+              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-white">
+                Legals
+              </h3>
+              <ul>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-white/60 hover:text-white"
+                    to="/"
+                  >
+                    Terms &amp; Conditions
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-white/60 hover:text-white"
+                    to="/"
+                  >
+                    Privacy Policy
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className=" text-base font-medium text-white/60 hover:text-white"
+                    to="/"
+                  >
+                    Licensing
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Footer
